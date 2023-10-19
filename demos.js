2b4c2aec75ce914215153903a590c69d2cb4ca69
Persist Computer <armon@persist-ai.com>
2023-10-18 18:38:08 -0700

    added some demo buttons to the runner example

---
+++ demos.js
@@ -0,0 +1,15 @@
+
+function fillFormTransfer(){
+    port.value="5000";
+    endpoint.value="transfer";
+    arg1.value=1;
+    arg2.value=2;
+    arg3.value=0.1;
+}
+
+function fillFormPlateMove(){
+    port.value="5001";
+    endpoint.value="move-to-well";
+    arg2.value=0;
+    arg2.value=0;
+}
