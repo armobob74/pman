fa612803769c22ace9114023867d73f1e20678fa
Armon <armon@nudgex.net>
2024-02-28 15:28:04 -0800

    added scheduler page that seems to work

--- PMAN_Server_Flask/website/pman_blueprints/release_scheduler.py
+++ PMAN_Server_Flask/website/pman_blueprints/release_scheduler.py
@@ -1,19 +1,21 @@
 from .aurora_pump import transfer_command_string, format_command, parse_response
-from .utils import extract_pman_args
-from flask import current_app, request, Blueprint
-from ..scheduler import add_job_to_scheduler
+from .csv_utils  import write_csv
+from flask import current_app, request, Blueprint, redirect
+from ..scheduler import  reload_scheduler
 
 release_scheduler = Blueprint('release_scheduler',__name__, url_prefix='/pman/release-scheduler')
 
-@release_scheduler.route("/transfer", methods=["POST"])
-@extract_pman_args
-def add_job(jobs):
+@release_scheduler.route("/save-table", methods=["POST"])
+def saveTable():
+    """ 
+    Save the table as a CSV, and also update the aptscheduler table 
+    Making an exception and not using PMAN arg formatting for this endpoint.
     """
-    jobs: a list of (job, datetime)
-    """
-    current_app.logger.debug(f"Called release scheduler add_job({jobs})")
-    volume = float(volume)
-    command_string = transfer_command_string(from_port, to_port, volume)
-    command = format_command(data=command_string) 
-    response = current_app.connection.send(command, immediate=True)
-    return {'status':'ok','message':parse_response(response)}
+    scheduler_tablepath = current_app.config['pman-config']['scheduler_tablepath']
+    data = request.json['data']
+    message = write_csv(data, scheduler_tablepath, '\t') 
+    reload_scheduler(current_app.scheduler, scheduler_tablepath)
+    if not message == True:
+        print(message) # it's an error message
+        return "There has been an error"
+    return redirect('/')
