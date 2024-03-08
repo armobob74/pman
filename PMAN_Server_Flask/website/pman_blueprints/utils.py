af55988f979276680d61e4bce360dac5d8941f55
Armon <armon@nudgex.net>
2024-03-07 19:06:28 -0800

    started work on aurora valve status parsing

--- PMAN_Server_Flask/website/pman_blueprints/utils.py
+++ PMAN_Server_Flask/website/pman_blueprints/utils.py
@@ -12,3 +12,40 @@ def extract_pman_args(f):
         args = data.get('args',[])
         return f(*args)
     return decorated_function
+
+def statusParserHamiltonAurora(statusChar):
+    """
+    Parse the status char in hamilton and aurora pumps 
+    This assumes that the Aurora uses the same status chars as Hamilton -- definitely double check that
+    """
+    busy_chars = '@ABCDFGIJKO'
+    ready_chars='`abcdfgijko'
+    if statusChar in busy_chars:
+        readiness = 'Busy'
+    elif statusChar in ready_chars:
+        readiness = 'Ready'
+    else:
+        return 'Unknown'
+
+    if statusChar == '@':
+        key = '`'
+    else:
+        key = statusChar.lower()
+    status_dict = {
+            '`':'No error',
+            'a':'Initialization error',
+            'b':'Invalid command',
+            'c':'Invalid operand',
+            'd':'Invalid command sequence',
+            'f':'EEPROM failure',
+            'g':'Syringe not initialized',
+            'i':'Syringe overload',
+            'j':'Valve overload',
+            'k':'Syringe move not allowed',
+            'o':'Pump is busy executing commands in the command buffer.'
+    }
+    status = f"{readiness}, {status_dict[key]}"
+    return status
+
+
+
