85bcd0adbe0ba19991e6f097d127f4285c3300e0
Armon <armon@nudgex.net>
2024-07-25 23:20:21 -0700

    added some logic for avoiding serial line collisions

--- PMAN_Server_Flask/website/pman_blueprints/dcdli_v2.py
+++ PMAN_Server_Flask/website/pman_blueprints/dcdli_v2.py
@@ -1,6 +1,9 @@
 """ DCDLI V2 is a relay board controlled over RS485 """
+import time
+import random 
 import pdb
 from flask import current_app, Blueprint, jsonify
+from minimalmodbus import InvalidResponseError
 from rs485_relay_board import RelayBoard
 from .utils import extract_pman_args
 
@@ -20,12 +23,24 @@ def get_relay_idx(relay_name):
         raise ValueError(f"Relay name {relay_name} not defined. Check spelling or update your config.")
     return relays[relay_name]
 
+def execute_serial_action(action,args=[]):
+    """ guarantees that the action is done, and redoes it if necessary """
+    response_good = False
+    while not response_good:
+        try:
+            action(*args)
+            response_good = True
+        except InvalidResponseError:
+            response_good = False
+            print(f"Got bad checksum, trying again")
+            time.sleep(random.random()/2) # the randomness prevents serial clogs
+
 @dcdli_v2.route('/on', methods=['POST'])
 @extract_pman_args
 def relay_on(relay_name):
     rb = make_relay()
     idx = get_relay_idx(relay_name)
-    rb.open_relay(idx)
+    execute_serial_action(rb.open_relay,[idx])
     return jsonify({'message':f'Relay {idx} ON', 'status':'ok'})
 
 @dcdli_v2.route('/off', methods=['POST'])
@@ -33,30 +48,40 @@ def relay_on(relay_name):
 def relay_off(relay_name):
     rb = make_relay()
     idx = get_relay_idx(relay_name)
-    rb.close_relay(idx)
+    execute_serial_action(rb.close_relay,[idx])
     return jsonify({'message':f'Relay {idx} OFF', 'status':'ok'})
 
 @dcdli_v2.get('/status/<string:relay_name>')
 def relay_status(relay_name):
     rb = make_relay()
     idx = get_relay_idx(relay_name)
-    return jsonify(rb.read_relay(idx))
+    # status updates do not get the special execution function because I do not want
+    # them to re-exeute in the event of a serial line collision
+    try:
+        return jsonify(rb.read_relay(idx))
+    except InvalidResponseError: 
+        return jsonify({'error': 'Invalid response from relay'}), 500
 
 @dcdli_v2.get('/status')
 def relay_status_all():
     rb = make_relay()
-    ret = rb.read_all_relays()
-    return jsonify(ret)
+    # status updates do not get the special execution function because I do not want
+    # them to re-exeute in the event of a serial line collision
+    try:
+        return jsonify(rb.read_all_relays())
+    except InvalidResponseError: 
+        return jsonify({'error': 'Invalid response from relays'}), 500
 
 @dcdli_v2.route('/all-on', methods=['POST'])
 def relay_all_on():
     rb = make_relay()
     rb.open_all()
+    execute_serial_action(rb.open_all)
     return jsonify({'message':f'Relays all ON', 'status':'ok'})
 
 @dcdli_v2.route('/all-off', methods=['POST'])
 def relay_all_off():
     rb = make_relay()
-    rb.close_all()
+    execute_serial_action(rb.close_all)
     return jsonify({'message':f'Relays all OFF', 'status':'ok'})
 
