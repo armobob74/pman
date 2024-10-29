aaa364a3e5b02d2c128c4769f68c84f64e794ef7
Armon <armon@persist-ai.com>
2024-10-29 15:48:47 -0700

    added error handling to busy check

--- PMAN_Server_Flask/website/pman_blueprints/aurora_valve.py
+++ PMAN_Server_Flask/website/pman_blueprints/aurora_valve.py
@@ -100,6 +100,8 @@ def isBusy(addr: int):
     command = base_command + cksum(base_command)
     response = current_app.connection.send(command, immediate=True)
     parsed_response = parse_response(response)
+    if parsed_response['status'] == "error":
+        return {'is-busy':'error'}
     if parsed_response['status'] == "Motor busy: The motor is currently executing another task.":
         return {'is-busy':True}
     else:
