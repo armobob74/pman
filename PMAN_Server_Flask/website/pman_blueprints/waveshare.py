9b31a5fd0408876f8fd5f3a6800a6e4c69ebda19
Persist thunder PC <armon.naddaf@gmail.com>
2025-01-03 16:32:49 -0800

    waveshare bugfix

--- PMAN_Server_Flask/website/pman_blueprints/waveshare.py
+++ PMAN_Server_Flask/website/pman_blueprints/waveshare.py
@@ -12,25 +12,25 @@ waveshare = Blueprint('waveshare',__name__, url_prefix='/pman/waveshare')
 @waveshare.route("/on", methods=["POST"])
 @extract_pman_args
 def turn_on(num):
-    current_app.connection.waveshare_relay_module.open_relay(num)
+    current_app.connection.waveshare.open_relay(num)
     return jsonify({"status": f"Relay {num} turned ON"})
 
 @waveshare.route("/off", methods=["POST"])
 @extract_pman_args
 def turn_off(num):
-    current_app.connection.waveshare_relay_module.close_relay(num)
+    current_app.connection.waveshare.close_relay(num)
     return jsonify({"status": f"Relay {num} turned ON"})
 
 @waveshare.route("/toggle", methods=["POST"])
 @extract_pman_args
 def toggle_channel(num):
-    current_state = current_app.connection.waveshare_relay_module.read_relay(num)
+    current_state = current_app.connection.waveshare.read_relay(num)
     new_state = not current_state
-    current_app.connection.waveshare_relay_module.instrument.write_bit(num-1, new_state, functioncode=5)
+    current_app.connection.waveshare.instrument.write_bit(num-1, new_state, functioncode=5)
     return jsonify({"status": f"Relay {num} toggled"})
 
 @waveshare.route("/all_status")
 def read_all():
-    rm = current_app.connection.waveshare_relay_module
+    rm = current_app.connection.waveshare
     relay_status = {i:('On' if rm.read_relay(i) else 'Off') for i in range(1,len(r)+1)}
     return relay_status
